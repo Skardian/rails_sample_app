106b035147754b9f78052d5d8ef633cf32f62656
Pepe Padial <jose.carlos.padial@gmail.com>
2016-08-27 18:40:46 +0200

    Log in the user on successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-     # Log in the user
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = "Invalid email/password combination"
       render 'new'
