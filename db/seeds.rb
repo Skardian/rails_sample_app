eb9cfb7bb34d7302ca861f2123d62eff86d7e386
Pepe Padial <jose.carlos.padial@gmail.com>
2016-08-29 11:18:31 +0200

    Add admin atribute to users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
 User.create!(name:  "Skardian",
              email: "jose.carlos.padial@gmail.com",
              password:              "foobar",
-             password_confirmation: "foobar")
+             password_confirmation: "foobar",
+             admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
